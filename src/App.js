7e693dc1ad24865c520c97bdbf34e4ea887b0683
Judicael <99041636+JrManjato@users.noreply.github.com>
2023-01-30 23:49:31 +0300

    feat: initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,95 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [list, setList] = useState([]);
+  const [newItem, setNewItem] = useState("");
+
+  function addItem(e) {
+    e.preventDefault();
+    // ! Check for empty item
+    if (!newItem) {
+      alert("Press enter an item.");
+      return;
+    }
+
+    const item = {
+      id: Date.now(),
+      value: newItem,
+      scale: 'todo'
+    };
+
+    // Add new item to items array
+    setList((oldList) => [...oldList, item]);
+
+    // Reset newItem back to original state
+    setNewItem("");
+  }
+
+  // Deletes an item based on the `item.id` key
+  function deleteItem(id) {
+    const newArray = list.filter((item) => item.id !== id);
+    setList(newArray);
+  }
+
+  function updateScale(id) {
+    // Get the current item
+    const currentItem = list.filter((item) => item.id === id);
+
+    // Create a new item with the updated scale
+    const newItem = {
+      id: currentItem[0].id,
+      value: currentItem[0].value,
+      scale: 'done'
+    };
+
+    deleteItem(id);
+
+    // Update the list
+    setList((oldList) => [...oldList, newItem]);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h1>Enter a task</h1>
+        <form
+          className="form col-md-10"
+          onSubmit={(e) => {
+            addItem(e);
+          }}
         >
-          Learn React
-        </a>
+          <input
+            type="text"
+            placeholder="Add a task..."
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+          />
+        </form>
+
+        <h1>To do</h1>
+        <ul>
+          {list.map((item) => item.scale === 'todo' && (
+            <li key={item.id}>
+              {item.value}
+              <input
+                type="checkbox"
+                onChange={() => updateScale(item.id)}
+              />
+            </li>
+          ))}
+        </ul>
+
+
+        <h1>Done</h1>
+        <ul>
+          {list.map((item) => item.scale === 'done' && (
+            <li key={item.id}>
+              {item.value}
+            </li>
+          ))}
+        </ul>
+
       </header>
     </div>
   );
